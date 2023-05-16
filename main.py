389fbafea45f6c9b114ce52223bb6a5da8a39a0d
WataNekko <88185666+WataNekko@users.noreply.github.com>
2023-05-16 16:01:08 +0700

    Add O(1) scheduler hehe

--- main.py
+++ main.py
@@ -6,14 +6,34 @@ from scheduler import *
 from task1 import *
 from task2 import *
 
-scheduler = Scheduler()
+# "O(n)" scheduler
+# scheduler = Scheduler()
+
+# "O(1)" scheduler on Update
+scheduler = Scheduler2()
+
 scheduler.SCH_Init()
+
+
+class TimerTask:
+    def __init__(self):
+        pass
+
+    def run(self):
+        print(f"At time: {self.timer}")
+        self.timer += 1
+
+    timer = 0
+
 
 task1 = Task1()
 task2 = Task2()
+timer_task = TimerTask()
+
 
 scheduler.SCH_Add_Task(task1.Task1_Run, 1000, 2000)
 scheduler.SCH_Add_Task(task2.Task2_Run, 2000, 4000)
+scheduler.SCH_Add_Task(timer_task.run, 0, 1000)
 
 while True:
     scheduler.SCH_Update()
--- scheduler.py
+++ scheduler.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 class Task:
     def __init__(self, _pTask, _Delay, _Period):
         self.pTask = _pTask
@@ -9,6 +12,7 @@ class Task:
     Period = 0
     RunMe = 0
     TaskID = -1
+
 
 class Scheduler:
     TICK = 1000
@@ -44,6 +48,64 @@ class Scheduler:
             if self.SCH_tasks_G[i].RunMe > 0:
                 self.SCH_tasks_G[i].RunMe -= 1
                 self.SCH_tasks_G[i].pTask()
+
+    def SCH_Delete(self, aTask):
+        return
+
+    def SCH_GenerateID(self):
+        return -1
+
+
+class Scheduler2:
+    TICK = 1000
+    SCH_MAX_TASKS = 40
+    SCH_tasks_G = deque()
+    current_index_task = 0
+
+    def __int__(self):
+        return
+
+    def SCH_Init(self):
+        self.current_index_task = 0
+
+    def SCH_Add_Task(self, pFunction, DELAY, PERIOD):
+        if self.current_index_task < self.SCH_MAX_TASKS:
+            aTask = Task(pFunction, DELAY / self.TICK, PERIOD / self.TICK)
+            aTask.TaskID = self.current_index_task
+            self._schedule_task(aTask)
+            self.current_index_task += 1
+        else:
+            print("PrivateTasks are full!!!")
+
+    def _schedule_task(self, task: Task):
+        i = 0
+        # find place to insert
+        for curr_task in self.SCH_tasks_G:
+            if (d := task.Delay - curr_task.Delay) > 0:
+                task.Delay = d
+            else:
+                curr_task.Delay -= task.Delay
+                self.SCH_tasks_G.insert(i, task)
+                return
+            i += 1
+
+        # insert at the end if cannot find place
+        self.SCH_tasks_G.append(task)
+
+    def SCH_Update(self):
+        if self.SCH_tasks_G[0].Delay > 0:
+            self.SCH_tasks_G[0].Delay -= 1
+
+    def SCH_Dispatch_Tasks(self):
+        while task := self.SCH_tasks_G[0]:
+            if task.Delay > 0:
+                return
+            # run then reschedule all ready tasks from the beginning of the deque
+            task.pTask()
+
+            self.SCH_tasks_G.popleft()
+            task.Delay = task.Period
+            self._schedule_task(task)
 
     def SCH_Delete(self, aTask):
         return
